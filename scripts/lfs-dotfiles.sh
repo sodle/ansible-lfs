c314e939abdc9f181793ba5fb1d07da9543518ab
Scott Odle <sjo@asu.edu>
2018-10-12 15:38:03 -0700

    actually nope

--- scripts/lfs-dotfiles.sh
+++ scripts/lfs-dotfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cat > ~/.bash_profile << EOF
+cat > ~/.bash_profile << "EOF"
 exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
 EOF
 
-cat > ~/.bashrc << EOF
+cat > ~/.bashrc << "EOF"
 set +h
 umask 022
 LFS=/mnt/lfs
