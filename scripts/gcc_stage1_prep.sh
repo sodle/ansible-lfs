50d39d647474171b5fdfa1dc88938a0a2211d473
Scott Odle <sjo@asu.edu>
2018-10-12 17:55:51 -0700

    build gcc

---
+++ scripts/gcc_stage1_prep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mv -v mpfr-4.0.1 mpfr
+mv -v gmp-6.1.2 gmp
+mv -v mpc-1.1.0 mpc
+
+for file in gcc/config/{linux,i386/linux{,64}}.h
+do
+  cp -uv $file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' $file.orig > $file
+  echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+  touch $file.orig
+done
+
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+ ;;
+esac
